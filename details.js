7697643fc6c9d69bce48f3b482c1cf962a8a3ae6
Emanuele Umberto Mangano <emanuelemangano15@gmail.com>
2024-04-11 16:59:44 +0200

    first commit

---
+++ details.js
@@ -0,0 +1,32 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+const URLimage = "https://api.pexels.com/v1/photos/" + id;
+
+fetch(URLimage, {
+  headers: {
+    Authorization: "PtBngZXlQHc8OFQeDYe2iVPr3FbiXSZ9KhfGntcukDS0RGTheGajt82u",
+  },
+})
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Fetch error");
+    }
+  })
+  .then((image) => {
+    const row = document.querySelector("#image-container");
+    const { photographer, photographer_url } = image;
+    const imgURL = image.src.large;
+    const img = document.createElement("div");
+    img.classList.add("d-flex", "flex-column", "align-items-center", "mt-5");
+    img.innerHTML = `
+      <img src="${imgURL}"/>
+      <p class="mt-3"><strong>Photographer:</strong> ${photographer}   </p>
+      <p><strong>Photographer personal page:</strong> ${photographer_url}   </p>
+        
+      
+      `;
+    row.appendChild(img);
+  });
---
+++ index.js
@@ -0,0 +1,86 @@
+const URLbasic = "https://api.pexels.com/v1/search?query=";
+const fetchGeneral = (URL) => {
+  fetch(URL, {
+    headers: {
+      Authorization: "PtBngZXlQHc8OFQeDYe2iVPr3FbiXSZ9KhfGntcukDS0RGTheGajt82u",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Fetch error");
+      }
+    })
+    .then((images) => {
+      const row = document.querySelector("#img-container");
+      row.innerHTML = "";
+      images.photos.forEach((img) => {
+        const { alt, id } = img;
+        const imgURL = img.src.medium;
+        const col = document.createElement("div");
+        col.classList.add("col");
+        const card = document.createElement("div");
+        card.classList.add("card", "mb-4", "shadow-sm");
+        card.innerHTML = `
+        <img src="${imgURL}" class="bd-placeholder-img card-img-top" style=" height: 200px; object-fit: cover;"  />
+                <div class="card-body ">
+                  <h5 class="card-title mb-5">${alt}</h5>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary hide">Hide</button>
+                    </div>
+                    <small class="text-muted">${id}</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+        
+        `;
+        card.addEventListener("click", () => {
+          window.location.href = "./details.html?id=" + id;
+        });
+        row.appendChild(col);
+        col.appendChild(card);
+      });
+
+      const hideBTN = document.querySelectorAll(".hide");
+      hideBTN.forEach((btn) => {
+        btn.addEventListener("click", () => {
+          btn.closest(".col").remove();
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const primaryBTN = document.querySelector("#primary-images");
+const LoadPrimaryImages = () => {
+  URLprimaryImages = "https://api.pexels.com/v1/search?query=dog";
+  fetchGeneral(URLprimaryImages);
+};
+
+const secondaryBTN = document.querySelector("#secondary-images");
+const LoadSecondaryImages = () => {
+  URLsecondaryImages = "https://api.pexels.com/v1/search?query=cars";
+  fetchGeneral(URLsecondaryImages);
+};
+
+const form = document.querySelector("form");
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const searchBar = document.querySelector("#search-bar");
+  const URLcustom = URLbasic + searchBar.value;
+  fetchGeneral(URLcustom);
+};
+
+form.addEventListener("submit", handleSubmit);
+
+window.addEventListener("DOMContentLoaded", () => {
+  firstURL = "https://api.pexels.com/v1/search?query=cat";
+  fetchGeneral(firstURL);
+});
